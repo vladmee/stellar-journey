28e2a40a2bc38d11cad686701517768595686529
Vlad Micliuc <vladpresedinte@gmail.com>
2023-06-02 18:28:58 +0800

    Add homepage

--- stellarjourney/src/app/(pages)/page.tsx
+++ stellarjourney/src/app/(pages)/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Navbar from '@/components/navbar/Navbar';
 import styles from './page.module.scss';
 
@@ -6,8 +7,19 @@ export default function Home() {
     <main className={styles['background-home']}>
       <Navbar />
       <div className={styles.home}>
-        <h1>Home</h1>
-        <h5>Some text to see how it looks</h5>
+        <div className={styles['home-info']}>
+          <h4>SO, YOU WANT TO TRAVEL TO</h4>
+          <h1>SPACE</h1>
+          <p>
+            Let&rsquo;s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we&rsquo;ll give you a truly out of
+            this world experience!
+          </p>
+        </div>
+        <div className={styles['home-btn']}>
+          <Link href={'/destination'}>Explore</Link>
+        </div>
       </div>
     </main>
   );
