c0f53059305fbea8b4392c1dc3c7054adf64bbd1
Vlad Micliuc <vladpresedinte@gmail.com>
2023-06-19 08:36:05 +0800

    Revert gh pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production';
-const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, './src/styles')],
-  },
-  assetPrefix: isProd ? repo : '',
-  images: {
-    unoptimized: true,
   },
 };
 
