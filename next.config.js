afae451369854b77fe46a05ce2d44758be39a2d0
Vlad Micliuc <vladpresedinte@gmail.com>
2023-06-19 08:24:25 +0800

    Add gh pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
 const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, './src/styles')],
+  },
+  assetPrefix: isProd ? repo : '',
+  images: {
+    unoptimized: true,
   },
 };
 
