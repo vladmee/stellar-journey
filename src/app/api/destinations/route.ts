753215a2645ed2b16e1cde9fb71e18acc01e29ad
Vlad Micliuc <vladpresedinte@gmail.com>
2023-06-19 09:01:18 +0800

    Update route

--- src/app/api/destinations/route.ts
+++ src/app/api/destinations/route.ts
@@ -4,9 +4,23 @@ import data from '@/data/data.json';
 import { Destination } from '@/data/types';
 
 export async function GET(req: Request) {
+  if (!data) {
+    console.error('Data not found');
+    return NextResponse.json({ message: 'Data not found' }, { status: 500 });
+  }
+
+  if (!data.destinations) {
+    console.error('Destinations not found');
+    return NextResponse.json(
+      { message: 'Destinations not found' },
+      { status: 500 }
+    );
+  }
+
   const pageData = data.destinations as Destination[];
 
-  if (pageData === undefined) {
+  if (!pageData) {
+    console.error('Page data not found');
     return NextResponse.json(
       { message: 'Page data not found' },
       { status: 500 }
